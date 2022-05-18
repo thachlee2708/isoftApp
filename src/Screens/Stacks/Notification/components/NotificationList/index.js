d0bfa05b1634f8bc6c714c4a66f8f113be779878
Thach Le <thachpro2708@gmail.com>
2022-05-18 15:10:39 +0700

    update

--- src/Screens/Stacks/Notification/components/NotificationList/index.js
+++ src/Screens/Stacks/Notification/components/NotificationList/index.js
@@ -17,38 +17,42 @@ const NotificationList = ({data}) => {
   const flatListRef = React.useRef();
   const onpressItem = React.useCallback(item => {}, []);
   const renderItems = React.useCallback(({item, index}) => {
+    const renderItemline = ({item, index}) => {
+      return (
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginVertical: pxScale.wp(10),
+          }}>
+          <AppImageSvg
+            source={item.icon}
+            width={pxScale.wp(30)}
+            height={pxScale.wp(30)}
+          />
+          <Text
+            numberOfLines={2}
+            style={[
+              styles.titleText,
+              {
+                fontFamily: item.read
+                  ? fontFamily.InterRegular
+                  : fontFamily.InterBold,
+              },
+            ]}>
+            {item.title}
+          </Text>
+        </View>
+      );
+    };
     return (
       <View style={{flex: 1}}>
         <Text>{formatDay(item[0].date)}</Text>
-        {item.map((item, index) => {
-          item.key = index.toString();
-          return (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginVertical: pxScale.wp(10),
-              }}>
-              <AppImageSvg
-                source={item.icon}
-                width={pxScale.wp(30)}
-                height={pxScale.wp(30)}
-              />
-              <Text
-                numberOfLines={2}
-                style={[
-                  styles.titleText,
-                  {
-                    fontFamily: item.read
-                      ? fontFamily.InterRegular
-                      : fontFamily.InterBold,
-                  },
-                ]}>
-                {item.title}
-              </Text>
-            </View>
-          );
-        })}
+        <FlatList
+          data={item}
+          renderItem={renderItemline}
+          keyExtractor={(_, index) => index.toString()}
+        />
       </View>
     );
   });
@@ -61,7 +65,7 @@ const NotificationList = ({data}) => {
         renderItem={renderItems}
         keyExtractor={(_, index) => index.toString()}
         contentContainerStyle={{
-          paddingBottom: pxScale.hp(35),
+          paddingBottom: pxScale.hp(80),
         }}
       />
     </View>
