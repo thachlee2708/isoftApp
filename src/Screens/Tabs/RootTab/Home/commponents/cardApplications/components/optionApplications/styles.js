e4ef43a297b8a40c0dc307998f5f2fc27f2b053e
Lê Ngọc Thạch <thachpro2708@gmail.com>
2022-06-13 10:42:00 +0700

    update

--- src/Screens/Tabs/RootTab/Home/commponents/cardApplications/components/optionApplications/styles.js
+++ src/Screens/Tabs/RootTab/Home/commponents/cardApplications/components/optionApplications/styles.js
@@ -2,10 +2,10 @@ import {StyleSheet} from 'react-native';
 import {colors} from 'constants';
 import {pxScale} from 'Helpers';
 export default StyleSheet.create({
-  container: {},
+  container: {marginBottom: pxScale.hp(10)},
   Imagecontainer: {
     flexDirection: 'row',
-    height: pxScale.hp(110),
+    height: pxScale.hp(100),
     width: pxScale.wp(190),
     paddingVertical: pxScale.hp(25),
     paddingHorizontal: pxScale.wp(15),
