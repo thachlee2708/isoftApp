413912ef426fcc3500484af85c258a18f390f8f4
LDAP\lnthach <thachln18411@st.uel.edu.vn>
2022-06-16 13:16:19 +0700

    update

--- src/Screens/Tabs/RootTab/Home/commponents/cardQR/index.js
+++ src/Screens/Tabs/RootTab/Home/commponents/cardQR/index.js
@@ -4,7 +4,40 @@ import styles from './styles';
 import AppImageSvg from 'components/AppImageSvg';
 import {AppIcon} from 'assets/icons';
 import {pxScale} from 'Helpers';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 const cardQR = () => {
+  const [img, setImg] = React.useState({
+    uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png',
+  });
+  const [data, setData] = React.useState(null);
+  const onChoiceImage = React.useCallback(
+    res => {
+      if (res?.assets?.length) {
+        setImg({uri: res.assets[0].uri});
+        RNQRGenerator.detect({
+          uri: res.assets[0].uri,
+        })
+          .then(response => {
+            const {values} = response; // Array of detected QR code values. Empty if nothing found.
+            console.log(values);
+          })
+          .catch(error => console.log('Cannot detect QR code in image', error));
+      } else {
+        return;
+      }
+    },
+    [setImg],
+  );
+  const onPressScanQr = React.useCallback(
+    async option => {
+      try {
+        launchImageLibrary(option, onChoiceImage);
+      } catch (e) {
+        console.log('error requestOpenCam', e);
+      }
+    },
+    [onChoiceImage],
+  );
   return (
     <View style={styles.container}>
       <TouchableOpacity activeOpacity={1}>
@@ -17,7 +50,9 @@ const cardQR = () => {
           <Text style={styles.text}>Scan QR</Text>
         </View>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={1}>
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => onPressScanQr({mediaType: 'photo'})}>
         <View style={styles.buttonContainer}>
           <AppImageSvg
             source={AppIcon.staffQR}
